2183a5595eb9d7b0880a257e3de4e7539b75f168
Jason Pitts <juniorjr083@gmail.com>
2018-10-13 02:09:57 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -65,7 +65,7 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters onFindPetsClick={this.props.onFindPetsClick} onChangeType={this.props.onChangeType}/>
+              <Filters onFindPetsClick={this.onFindPetsClick} onChangeType={this.onChangeType}/>
             </div>
             <div className="twelve wide column">
               <PetBrowser />
